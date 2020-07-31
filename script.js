78e29bed9f1f0055d0bd5006d51eef01bb575889
Visethboti <visethboti@gmail.com>
2020-07-31 16:01:41 +0700

    Done with Map

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-// // document.getElementsById('map').innerHTML = "<p>hello world</p>"
-// alert("hello");
-// document.getElementsById("map").innerHTML = "hello world";
-// alert("hello2");
 var edge = "<div class='edge'></div>";
 var empty = "<div class='empty'></div>";
 var snakeHead = "<div class='snakeHead'></div>";
@@ -15,24 +11,57 @@ var gameMap = document.querySelector('#map');
 var status = document.querySelector('#status');
 var mapData = ""
 
-var foodX = Math.floor(Math.random() * (width-2)) + 1;
-var foodY = Math.floor(Math.random() * (height-2)) + 1;
+var foodX = 1;
+var foodY = 1;
 var foodLoc = "x:"+foodX+"|y:"+foodY;
 
+var snakeX;
+var snakeY;
+var snakeBodyLoc;
+
+var gameOver;
+
+//////////////// Functions //////////////////////////
+
+function setup(){
+  foodX = Math.floor(Math.random() * (width-2)) + 1;
+  foodY = Math.floor(Math.random() * (height-2)) + 1;
+
+  snakeX = width/2;
+  snakeY = height/2;
+
+  gameOver = false;
+}
+
 function drawMap(){
-  //gameMap.innerHTML = edge;
   for(var y = 0; y < height; y++){
     for(var x = 0; x < width; x++){
       if(x==0 || x == width-1 || y == 0 || y == height-1)
         mapData += edge;
+      else if (x == snakeX && y == snakeY)
+        mapData += snakeHead;
       else if (x == foodX && y == foodY)
         mapData += food;
       else
         mapData += empty;
     }
-    mapData += "<br>";
   }
 
   gameMap.innerHTML = mapData;
   status.innterHTML = foodLoc;
+}
+
+function move(){
+
+}
+
+function check(){
+
+}
+
+//////////////////////////////////////////////////////
+
+function game(){
+  setup();
+  drawMap();
 }
